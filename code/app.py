6091b63a3269bbfbbb4c817139a9b00faccc79a6
ucyo <cayoglu@me.com>
2021-10-08 16:56:08 +0200

    Refactor and add selection for germany

--- code/app.py
+++ code/app.py
@@ -1,5 +1,6 @@
+import intake
 from satsearch import Search
-from const import germany_bbox
+from const import germany_bbox, selection
 
 cloud_coverage = 5  # 0 - 100%
 data_coverage = 80  # 0 - 100%
@@ -8,16 +9,21 @@ URL = "https://earth-search.aws.element84.com/v0"
 
 
 def main():
+    catalog = get_intake_catalog(dates, germany_bbox, cloud_coverage, data_coverage)
+
+
+def get_intake_catalog(dates, bbox, cc, dc):
     results = Search.search(
         url=URL,
         collections=["sentinel-s2-l2a-cogs"],  # note collection='sentinel-s2-l2a-cogs' doesn't work
         datetime=dates,
-        bbox=germany_bbox,
-        query=[f"eo:cloud_cover<{cloud_coverage}", f"sentinel:data_coverage>{data_coverage}"],
+        bbox=bbox,
+        query=[f"eo:cloud_cover<{cc}", f"sentinel:data_coverage>{dc}"],
         sort=["<datetime"],
     )
-    print("%s items" % results.found())
+    print(f"{results.found()} items")
     items = results.items()
+    return intake.open_stac_item_collection(items)
 
 
 if __name__ == "__main__":
