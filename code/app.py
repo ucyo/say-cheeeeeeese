25164643f2a1fbbc8e197743e08f5b60a67170a0
ucyo <cayoglu@me.com>
2021-10-08 16:49:13 +0200

    Search for sentinel-2 data using date, bbox, cloud coverage and data coverage

--- code/app.py
+++ code/app.py
@@ -1,5 +1,23 @@
+from satsearch import Search
+from const import germany_bbox
+
+cloud_coverage = 5  # 0 - 100%
+data_coverage = 80  # 0 - 100%
+dates = "2017-01-01/2018-05-01"
+URL = "https://earth-search.aws.element84.com/v0"
+
+
 def main():
-    print(f"It works! 1 + 1 = {1 + 1}")
+    results = Search.search(
+        url=URL,
+        collections=["sentinel-s2-l2a-cogs"],  # note collection='sentinel-s2-l2a-cogs' doesn't work
+        datetime=dates,
+        bbox=germany_bbox,
+        query=[f"eo:cloud_cover<{cloud_coverage}", f"sentinel:data_coverage>{data_coverage}"],
+        sort=["<datetime"],
+    )
+    print("%s items" % results.found())
+    items = results.items()
 
 
 if __name__ == "__main__":
---
+++ code/const.py
@@ -0,0 +1,1 @@
+germany_bbox = [6.95, 47.39, 14.56, 54.31]  # (min lon, min lat, max lon, max lat)
